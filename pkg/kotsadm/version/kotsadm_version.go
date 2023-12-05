c43f07cb0c554c8b1a325b3fdb008088501805e9
Alexander Dejanovski <adejanovski@gmail.com>
2023-12-05 13:43:00 +0100

    Use v1.99.0 as tag for the kotsadm image

--- pkg/kotsadm/version/kotsadm_version.go
+++ pkg/kotsadm/version/kotsadm_version.go
@@ -28,11 +28,11 @@ func KotsadmTag(registryConfig types.RegistryConfig) string {
 func KotsadmTagForVersionString(kotsVersion string) string {
 	version, err := semver.NewVersion(kotsVersion)
 	if err != nil {
-		return "alpha"
+		return "v1.99.0"
 	}
 
 	if strings.Contains(version.Prerelease(), "dirty") {
-		return "alpha"
+		return "v1.99.0"
 	}
 
 	if !strings.HasPrefix(kotsVersion, "v") {
